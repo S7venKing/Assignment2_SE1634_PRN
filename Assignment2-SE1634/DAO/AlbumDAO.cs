f973231aabd46959a142355d56736d57b2f3085b
S7venKing <3r4n01d@gmail.com>
2022-10-13 19:26:08 +0700

    MainGUI + ShoppingGUI +LoginGUI

---
+++ Assignment2-SE1634/DAO/AlbumDAO.cs
@@ -0,0 +1,33 @@
+﻿using Assignment2_SE1634.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_SE1634.DAO
+{
+    internal class AlbumDAO
+    {
+        public List<Album> LoadAllAlbum()
+        {
+            var list = new List<Album>();
+            MusicStoreContext musicStore = new MusicStoreContext();
+            list = musicStore.Albums.ToList();
+            return list;
+        }
+        public List<Album> LoadPaginationAlbum(int page,int quantity, List<Album> listA)
+        {
+            var listPage = listA.Skip((page-1) * quantity).Take(quantity).ToList();
+            return listPage;
+        }
+
+        public List<Album> LoadAlbumByGenre(int genreID)
+        {
+            var list = new List<Album>();
+            list = LoadAllAlbum();
+            var listPage = list.Where(p=>p.GenreId==genreID).ToList();
+            return listPage;
+        }
+    }
+}
---
+++ Assignment2-SE1634/DAO/ArtistDAO.cs
@@ -0,0 +1,20 @@
+﻿using Assignment2_SE1634.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_SE1634.DAO
+{
+    internal class ArtistDAO
+    {
+        public Artist GetArtistByID(int Id)
+        {
+            Artist artist = new Artist();
+            MusicStoreContext musicStoreContext = new MusicStoreContext();
+            artist = musicStoreContext.Artists.ToList().Where(p => p.ArtistId == Id).FirstOrDefault();
+            return artist;
+        }
+    }
+}
